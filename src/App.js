7c2002d3ae745611d46b7dc1122a58491950075b
Isaac Muniz <codigoisaac@protonmail.com>
2021-04-05 13:25:59 -0300

    Update reminder in the server

--- src/App.js
+++ src/App.js
@@ -11,10 +11,21 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(updTask),
+    });
+
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
@@ -25,9 +36,7 @@ function App() {
       headers: { "Content-type": "application/json" },
       body: JSON.stringify(task),
     });
-
     const data = await res.json();
-
     setTasks([...tasks, data]);
   };
 
@@ -43,6 +52,12 @@ function App() {
 
   const fetchTasks = async () => {
     const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+    return data;
+  };
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
     const data = await res.json();
     return data;
   };
