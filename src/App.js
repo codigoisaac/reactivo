a81aa0c511fa96c2287ac090b16ddff1882edf64
Isaac Muniz <codigoisaac@yahoo.com>
2021-04-05 11:57:50 -0300

    Add json-server

--- src/App.js
+++ src/App.js
@@ -4,26 +4,7 @@ import { useState } from 'react'
 import AddTask from './components/AddTask'
 
 function App() {
-  const [tasks, setTasks] = useState([
-		{
-			id: 1,
-			text: 'Make Money',
-			date: 'Apr 29th at 2:30pm',
-			reminder: false,
-		},
-		{
-			id: 2,
-			text: 'Make Food',
-			date: 'Apr 25th at 2:30pm',
-			reminder: true,
-		},
-		{
-			id: 3,
-			text: 'Make Love',
-			date: 'Set 13th at 5:30am',
-			reminder: false,
-		}
-	]);
+  const [tasks, setTasks] = useState([]);
 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
