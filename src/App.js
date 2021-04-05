5ec082537fd9ec956cf05f91a666ed614414ec0b
Isaac Muniz <codigoisaac@yahoo.com>
2021-04-05 11:12:23 -0300

    Build for production

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ function App() {
 		{
 			id: 1,
 			text: 'Make Money',
-			day: 'Apr 29th at 2:30pm',
+			date: 'Apr 29th at 2:30pm',
 			reminder: false,
 		},
 		{
 			id: 2,
 			text: 'Make Food',
-			day: 'Apr 25th at 2:30pm',
+			date: 'Apr 25th at 2:30pm',
 			reminder: true,
 		},
 		{
 			id: 3,
 			text: 'Make Love',
-			day: 'Set 13th at 5:30am',
+			date: 'Set 13th at 5:30am',
 			reminder: false,
 		}
 	]);
