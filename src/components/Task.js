026c3ced2ffefe0c7c9fcb1228f0c32715608940
Isaac Muniz <codigoisaac@protonmail.com>
2021-04-09 15:00:23 -0300

    Change task style

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,14 +4,14 @@ const Task = ({ task, onDelete, onToggle }) => {
   return (
     <div className={`task ${task.important ? "important" : ""}`}>
       <div className='taskHeader'>
-        <input onClick={() => onToggle(task.id)} type="checkbox" id="importantCheck" style={{ cursor: 'cell' }}/>
         <h3>{task.text}</h3>
+        <input onClick={() => onToggle(task.id)} type="checkbox" id="importantCheck" />
         <FaTimes
-            style={{ color: "red" }}
+            style={{ color: "red", cursor: 'pointer' }}
             onClick={() => onDelete(task.id)}
           />
       </div>
-      <p>{task.date}</p>
+      {task.date && <p>{task.date}</p>}
     </div>
   );
 };
